dca7a241eca22c89a077ec3405cb353f8301e576
Fraser Russell <fraser.russell1991@gmail.com>
2023-09-26 11:31:24 +0100

    3d image now adjusts with screen size when used with a mobile

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -3,7 +3,7 @@ import { Canvas } from '@react-three/fiber'
 import { OrbitControls, Preload, useGLTF } from '@react-three/drei'
 import CanvasLoader from '../Loader'
 
-const Computers = () => {
+const Computers = ( {isMobile }) => {
   const computer = useGLTF('./desktop_pc/scene.gltf')
   
   return (
@@ -13,14 +13,14 @@ const Computers = () => {
       <pointLight intensity={2} />
       <spotLight
         penumbra={1}
-        intensity={10}
+        intensity={100}
         castShadow
         shadow-mapSize={1024}
       />
       <primitive 
         object={computer.scene}
-        scale={0.65}
-        position={[0, -3.25, -1.5]}
+        scale={isMobile ? 0.35 : 0.65}
+        position={isMobile ? [0, -1.5, -0.75] : [0, -3.25, -1.5]}
         rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
@@ -28,6 +28,26 @@ const Computers = () => {
 }
 
 const ComputersCanvas = () => {
+  const [isMobile, setIsMobile] = useState(false)
+
+  useEffect(() => {
+    //assume it's a mobile if the max-width is 500 or less
+    const mediaQuery = window.matchMedia('(max-width: 500px)')
+
+    setIsMobile(mediaQuery.matches)
+
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches)
+    }
+
+    //listen to mediaQuery changing and run handleMediaQueryChange funtion
+    mediaQuery.addEventListener('change', handleMediaQueryChange)
+
+    return () => {
+      mediaQuery.removeEventListener('change', handleMediaQueryChange)
+    }
+  }, [])
+
   return (
     <Canvas
       frameloop="demand"
@@ -39,7 +59,7 @@ const ComputersCanvas = () => {
           enableZoom={false}
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}/>
-          <Computers />
+          <Computers isMobile={isMobile}/>
         </Suspense>
           <Preload all/>
     </Canvas>
